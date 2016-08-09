34f5c6930cd63b49c9bd54ee33d9b0d56d16476a
Viktor S <brutalic@gmail.com>
2016-08-08 18:49:06 -0700

    New directory /class2 and short script displaying the paramiko and pysnmp versions

---
+++ class2/ParamikoPysnmpVersion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import pysnmp
+import paramiko
+
+print "\nPysnmp is using version: "
+print pysnmp.version
+
+print "\nParamiko is using version: "
+print paramiko.__version__
+print "\n"
+
+
